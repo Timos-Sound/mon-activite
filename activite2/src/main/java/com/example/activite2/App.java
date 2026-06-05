1da10deb6db3f27224682b27c44e8bae9e446c14
Timos-Sound <tel_bonzi@yahoo.fr>
2026-06-05 04:26:42 +0100

    Update App.java

--- activite2/src/main/java/com/example/activite2/App.java
+++ activite2/src/main/java/com/example/activite2/App.java
@@ -6,7 +6,7 @@ package com.example.activite2;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args )
     {
         System.out.println( "Hello World!" );
     }

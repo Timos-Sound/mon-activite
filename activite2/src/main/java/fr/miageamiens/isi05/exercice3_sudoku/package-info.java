6bd875b31e07c20f12d0b6bfa97f2db035e371d8
Timos-Sound <tel_bonzi@yahoo.fr>
2026-06-07 21:17:05 +0100

    Update package-info.java

--- activite2/src/main/java/fr/miageamiens/isi05/exercice3_sudoku/package-info.java
+++ activite2/src/main/java/fr/miageamiens/isi05/exercice3_sudoku/package-info.java
@@ -1,4 +1,4 @@
 /**
  * Package contenant les exceptions personnalisées de l'application Sudoku.
  */
-package fr.miageamiens.isi05.sudoku.modele.exceptions;
+package fr.miageamiens.isi05.sudoku.modele.exercice3_sudoku;
